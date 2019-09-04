36cbcd396747615404a1641175e2e82542699967
PatrycjaKarbownik <patrycja@karbownik.net>
2019-09-04 22:37:37 +0200

    refactor: change classes, methods and variables naming

--- src/main/scala/org/biodatageeks/CustomFunctions.scala
+++ src/main/scala/org/biodatageeks/CustomFunctions.scala
@@ -1,25 +1,25 @@
 package org.biodatageeks
-import org.apache.spark.{Partition, TaskContext}
-import org.apache.spark.rdd.{BDGVCFPipedRDD, RDD}
-import htsjdk.variant.variantcontext.VariantContext
+
 import org.apache.hadoop.io.Text
-import org.seqdoop.hadoop_bam.VariantContextWritable
+import org.apache.spark.rdd.{RDD, VCFPipedRDD}
 
 import scala.collection.Map
 
-class CustomFunctions(rdd:RDD[Text]) {
+class CustomFunctions(rdd: RDD[Text]) {
 
-
-  def pipeVCF(command: String, env: Map[String, String] = Map.empty) = new BDGVCFPipedRDD(rdd,BDGVCFPipedRDD.tokenize(command),env,
-    null,
-    null,
-    true,
-    encoding = "UTF-8",
-    bufferSize = 8192)
+  def process(command: String, env: Map[String, String] = Map.empty) =
+    new VCFPipedRDD(
+      rdd,
+      VCFPipedRDD.tokenize(command),
+      env,
+      null,
+      null,
+      true,
+      encoding = "UTF-8",
+      bufferSize = 8192)
 
 }
 
 object CustomFunctions {
-
   implicit def addCustomFunctions(rdd: RDD[Text]) = new CustomFunctions(rdd)
 }
