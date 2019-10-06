9d452482094b8a0fdf3fa1cf6641fe590a34dba9
PatrycjaKarbownik <patrycja@karbownik.net>
2019-10-06 13:25:29 +0200

    other: change test name, add more variants to test file

--- src/test/scala/org/biodatageeks/VCFTest.scala
+++ src/test/scala/org/biodatageeks/VCFTest.scala
@@ -17,28 +17,26 @@ class VCFTest extends FunSuite with BeforeAndAfter with PrivateMethodTester {
 
   val inputPath = getClass.getClassLoader.getResource("vcfTest.vcf").getPath
 
-  test("should make rdds on 2 partitions") {
+  before {
     sparkSession.sparkContext.hadoopConfiguration.setStrings("io.compression.codecs",
       classOf[BGZFCodec].getCanonicalName,
       classOf[BGZFEnhancedGzipCodec].getCanonicalName)
+  }
 
+  test("should make rdds on 2 partitions") {
     val rdds = SeqTenderVCF.makeVCFRDDs(sparkSession, inputPath)
 
     assert(rdds.getNumPartitions === 2)
   }
 
-  test("should return true when each partition (except first) has the headerLines") {
-    sparkSession.sparkContext.hadoopConfiguration.setStrings("io.compression.codecs",
-      classOf[BGZFCodec].getCanonicalName,
-      classOf[BGZFEnhancedGzipCodec].getCanonicalName)
-
+  test("should return true when each partition has the headerLines - header was added to partitions in makeVCFRDDs method") {
     val headerLines =
       "##fileformat=VCFv4.2\n##FILTER=<ID=PASS,Description=\"All filters passed\">\n##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n##INFO=<ID=AF,Number=A,Type=Float,Description=\"Allele Frequency, for each ALT allele, in the same order as listed\">\n##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">\n##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">\n##contig=<ID=11,assembly=b37,length=135006516>\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample"
 
     val rdds = SeqTenderVCF.makeVCFRDDs(sparkSession, inputPath)
-    val containHeader = sparkSession.sparkContext.accumulator(0, "containHeader")
+    val notContainHeader = sparkSession.sparkContext.accumulator(0, "notContainHeader")
     rdds.foreachPartition(it => {
-      // we don't have to check first partition, because it always has the header
+      // we don't have to check first partition, because it always has the header (by default)
       if (TaskContext.getPartitionId != 0) {
         var file = ArrayBuffer[String]()
         it.map(_.toString).foreach(it => {
@@ -46,11 +44,11 @@ class VCFTest extends FunSuite with BeforeAndAfter with PrivateMethodTester {
         })
 
         if (!file.contains(headerLines))
-          containHeader.add(1)
+          notContainHeader.add(1)
       }
     })
 
-    assert(containHeader.value === 0)
+    assert(notContainHeader.value === 0)
   }
 
   test("should return number of elements in vcf rdd - number of lines in input file (without header)") {
@@ -60,6 +58,6 @@ class VCFTest extends FunSuite with BeforeAndAfter with PrivateMethodTester {
         "docker run --rm -i biodatageeks/bdg-vt:latest vt decompose - ",
         sparkSession)
 
-    assert(vc.count() === 2)
+    assert(vc.count() === 20)
   }
 }
