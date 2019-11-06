91bb761c820518f08082f410a96cf53050e0814b
PatrycjaKarbownik <patrycja@karbownik.net>
2019-11-06 22:33:39 +0100

    feat: add save output to sam file

--- src/main/scala/org/biodatageeks/alignment/TestFQInOut.scala
+++ src/main/scala/org/biodatageeks/alignment/TestFQInOut.scala
@@ -1,6 +1,8 @@
 package org.biodatageeks.alignment
 
-import org.apache.hadoop.fs.{FileSystem, Path}
+import java.io.File
+
+import htsjdk.samtools.{SAMRecord, SAMTextWriter}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -22,9 +24,9 @@ object TestFQInOut {
 
     // /home/patrycja/Pulpit/Praca_inzynierska/00_Seqtender/data/bowtie2_index/e_coli
     val command = "docker run --rm -i " +
-                  "-v /home/patrycja/Pulpit/Praca_inzynierska/00_Seqtender/data/:/data " +
-                  "quay.io/biocontainers/bowtie2:2.3.4.3--py27h2d50403_0 " +
-                  "bowtie2 -x /data/bowtie2_index/e_coli - "/* + args(1)*/ + " "
+      "-v /home/patrycja/Pulpit/Praca_inzynierska/00_Seqtender/data/:/data " +
+      "quay.io/biocontainers/bowtie2:2.3.4.3--py27h2d50403_0 " +
+      "bowtie2 -x /data/bowtie2_index/e_coli - " /* + args(1)*/ + " "
 
     val alignment = SeqTenderAlignment
       .pipeReads(
@@ -34,15 +36,21 @@ object TestFQInOut {
       )
 
     //alignment.map(_.toString).collect().foreach(line => println(line))
-    sparkSession.time(println(alignment.count()))
+    //println(alignment.take(2).head.toString)
+    //    sparkSession.time(println(alignment.count()))
 
-    // saveRddToFile(sparkSession, alignment, args(2))
+    saveRddToFile(sparkSession, alignment, args(2))
+//    println(alignment.first().getHeader())
   }
 
-  def saveRddToFile[T: ClassTag](sparkSession: SparkSession, rdd: RDD[T], pathWrite: String): Unit = {
-    val fileSystem = FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)
+  def saveRddToFile[T: ClassTag](sparkSession: SparkSession, rdd: RDD[SAMRecord], pathWrite: String): Unit = {
+    /*val fileSystem = FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)
     val outputStream = fileSystem.create(new Path(pathWrite))
     rdd.map(_.toString).collect().foreach(line => outputStream.writeBytes(s"\n$line"))
-    outputStream.close()
+    outputStream.close()*/
+    val samTextWriter = new SAMTextWriter(new File(pathWrite))
+    samTextWriter.writeHeader(rdd.first().getHeader.getTextHeader)
+    rdd.collect().foreach(record => samTextWriter.writeAlignment(record))
+    samTextWriter.close();
   }
 }
