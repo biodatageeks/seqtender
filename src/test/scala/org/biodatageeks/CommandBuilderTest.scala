06aa1d9848fa8ecd62ba353955e1d0e63657d0fc
PatrycjaKarbownik <patrycja@karbownik.net>
2019-12-01 22:50:58 +0100

    test: add fq/fa/ifq alignment tests

--- src/test/scala/org/biodatageeks/CommandBuilderTest.scala
+++ src/test/scala/org/biodatageeks/CommandBuilderTest.scala
@@ -4,41 +4,17 @@ import org.biodatageeks.alignment.{CommandBuilder, Constants}
 import org.scalatest.FunSuite
 
 class CommandBuilderTest extends FunSuite {
-  val dataDirectory = "alignment"
-  val readsDirectory = s"${dataDirectory}/reads"
-
-  val referenceGenomePath: String = getClass.getClassLoader.getResource(s"${dataDirectory}/bwa_index/e_coli_short.fa").getPath
-  val bwaIndex: String = referenceGenomePath
-  val bwaIndexDirectory: String = bwaIndex.splitAt(bwaIndex.lastIndexOf("/") + 1)._1
-
-  val bowtieIndex: String = changeIndexPath("bowtie_index")
-  val bowtieIndexDirectory: String = bowtieIndex.splitAt(bowtieIndex.lastIndexOf("/") + 1)._1
-
-  val bowtie2Index: String = changeIndexPath("bowtie2_index")
-  val bowtie2IndexDirectory: String = bowtie2Index.splitAt(bowtie2Index.lastIndexOf("/") + 1)._1
-
-  val faReadsPath: String = getClass.getClassLoader.getResource(s"${readsDirectory}/e_coli_13.fa").getPath
-  val fqReadsPath: String = getClass.getClassLoader.getResource(s"${readsDirectory}/e_coli_13.fq").getPath
-  val ifqReadsPath: String = getClass.getClassLoader.getResource(s"${readsDirectory}/e_coli_13.ifq").getPath
-
-  // it is necessary, because we can't get a path to nonspecific file
-  private def changeIndexPath(finalDirectory: String): String = {
-    val tmp = referenceGenomePath.replace("bwa_index", finalDirectory)
-    val splitAt = tmp.length - 3
-    tmp.splitAt(splitAt)._1
-  }
-
 
   test("should make correct bowtie2 command to align fq reads") {
     val commandBuilder = new CommandBuilder(
-      readsPath = fqReadsPath,
-      indexPath = bowtie2Index,
+      readsPath = InputPaths.fqReadsPath,
+      indexPath = InputPaths.bowtie2Index,
       tool = Constants.bowtie2ToolName
     )
 
     val correctCommand = "docker run --rm -i " +
-      s"-v $bowtie2IndexDirectory:/data " +
-      Constants.defaultBowtie2Image +
+      s"-v ${InputPaths.bowtie2IndexDirectory}:/data " +
+      s"${Constants.defaultBowtie2Image} " +
       "bowtie2 -x " +
       "/data/e_coli_short " +
       "- "
@@ -48,15 +24,15 @@ class CommandBuilderTest extends FunSuite {
 
   test("should make correct bowtie2 command to align interleaved fq reads") {
     val commandBuilder = new CommandBuilder(
-      readsPath = ifqReadsPath,
-      indexPath = bowtie2Index,
+      readsPath = InputPaths.ifqReadsPath,
+      indexPath = InputPaths.bowtie2Index,
       tool = Constants.bowtie2ToolName,
       interleaved = true
     )
 
     val correctCommand = "docker run --rm -i " +
-      s"-v $bowtie2IndexDirectory:/data " +
-      Constants.defaultBowtie2Image +
+      s"-v ${InputPaths.bowtie2IndexDirectory}:/data " +
+      s"${Constants.defaultBowtie2Image} " +
       "bowtie2 -x /data/e_coli_short " +
       "--interleaved " +
       "- "
@@ -66,14 +42,14 @@ class CommandBuilderTest extends FunSuite {
 
   test("should make correct bowtie2 command to align fa reads") {
     val commandBuilder = new CommandBuilder(
-      readsPath = faReadsPath,
-      indexPath = bowtie2Index,
+      readsPath = InputPaths.faReadsPath,
+      indexPath = InputPaths.bowtie2Index,
       tool = Constants.bowtie2ToolName
     )
 
     val correctCommand = "docker run --rm -i " +
-      s"-v $bowtie2IndexDirectory:/data " +
-      Constants.defaultBowtie2Image +
+      s"-v ${InputPaths.bowtie2IndexDirectory}:/data " +
+      s"${Constants.defaultBowtie2Image} " +
       "bowtie2 -x " +
       "/data/e_coli_short " +
       "-f " +
--- src/test/scala/org/biodatageeks/FQTest.scala
+++ src/test/scala/org/biodatageeks/FQTest.scala
@@ -1,18 +1,18 @@
 package org.biodatageeks
 
+import htsjdk.samtools.SAMRecord
 import org.apache.spark.sql.SparkSession
-import org.biodatageeks.alignment.SeqTenderAlignment
+import org.biodatageeks.alignment.{CommandBuilder, Constants, SeqTenderAlignment}
 import org.scalatest.{BeforeAndAfter, FunSuite, PrivateMethodTester}
 import org.seqdoop.hadoop_bam.util.{BGZFCodec, BGZFEnhancedGzipCodec}
 
+// todo: read about PrivateMethodTester
 class FQTest extends FunSuite with BeforeAndAfter with PrivateMethodTester {
 
-  val sparkSession = SparkSession
+  val sparkSession: SparkSession = SparkSession
     .builder()
     .master("local[2]")
     .getOrCreate()
-
-  val inputPath = getClass.getClassLoader.getResource("vcfTest.vcf").getPath
 
   before {
     sparkSession.sparkContext.hadoopConfiguration.setStrings("io.compression.codecs",
@@ -20,11 +20,67 @@ class FQTest extends FunSuite with BeforeAndAfter with PrivateMethodTester {
       classOf[BGZFEnhancedGzipCodec].getCanonicalName)
   }
 
-  test("should make rdds on 2 partitions") {
-    sparkSession.sparkContext.hadoopConfiguration.setInt("mapred.max.split.size", 40000)
+  test("should make fastq rdds on 2 partitions") {
+    sparkSession.sparkContext.hadoopConfiguration.setInt("mapred.max.split.size", 500)
+    val readsDescription = new CommandBuilder(
+      readsPath = InputPaths.fqReadsPath,
+      indexPath = InputPaths.bowtie2Index,
+      tool = Constants.bowtie2ToolName
+    )
 
-    val rdds = SeqTenderAlignment.makeReadRddsFromFQ(sparkSession, inputPath)
+    val rdds = SeqTenderAlignment.makeReadRddsFromFQ(sparkSession, readsDescription.getReadsPath)
 
     assert(rdds.getNumPartitions === 2)
+  }
+
+  test("should make fasta rdds on 2 partitions") {
+    val readsDescription = new CommandBuilder(
+      readsPath = InputPaths.faReadsPath,
+      indexPath = InputPaths.bowtie2Index,
+      tool = Constants.bowtie2ToolName
+    )
+
+    val rdds = SeqTenderAlignment.makeReadRddsFromFA(sparkSession, readsDescription.getReadsPath)
+
+    assert(rdds.getNumPartitions === 2)
+  }
+
+  test("should return number of aligned and unaligned fastq reads") {
+    val readsDescription = new CommandBuilder(
+      readsPath = InputPaths.fqReadsPath,
+      indexPath = InputPaths.bowtie2Index,
+      tool = Constants.bowtie2ToolName
+    )
+
+    val sam = SeqTenderAlignment.pipeReads(readsDescription, sparkSession)
+
+    assert(sam.collect.count(it => it.getAlignmentStart !== SAMRecord.NO_ALIGNMENT_START) === 10)
+    assert(sam.collect.count(it => it.getAlignmentStart === SAMRecord.NO_ALIGNMENT_START) === 3)
+  }
+
+  test("should return number of aligned and unaligned fasta reads") {
+    val readsDescription = new CommandBuilder(
+      readsPath = InputPaths.faReadsPath,
+      indexPath = InputPaths.bowtie2Index,
+      tool = Constants.bowtie2ToolName
+    )
+
+    val sam = SeqTenderAlignment.pipeReads(readsDescription, sparkSession)
+
+    assert(sam.collect.count(it => it.getAlignmentStart !== SAMRecord.NO_ALIGNMENT_START) === 10)
+    assert(sam.collect.count(it => it.getAlignmentStart === SAMRecord.NO_ALIGNMENT_START) === 3)
+  }
+
+  test("should return number of aligned and unaligned interleaved fastq reads") {
+    val readsDescription = new CommandBuilder(
+      readsPath = InputPaths.ifqReadsPath,
+      indexPath = InputPaths.bowtie2Index,
+      tool = Constants.bowtie2ToolName
+    )
+
+    val sam = SeqTenderAlignment.pipeReads(readsDescription, sparkSession)
+
+    assert(sam.collect.count(it => it.getAlignmentStart !== SAMRecord.NO_ALIGNMENT_START) === 20)
+    assert(sam.collect.count(it => it.getAlignmentStart === SAMRecord.NO_ALIGNMENT_START) === 6)
   }
 }
